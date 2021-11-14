cd9e92b4ef278a56e877a8b8ea30f538fda76094
1910598john <1910598john@gmail.com>
2021-11-14 11:54:45 +0800

    Fixed some issues

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 $(document).ready(function(){
-    document.getElementById("gallery").addEventListener("click", gallery);
+    document.getElementById("photos").addEventListener("click", photos);
+    document.getElementById("videos").addEventListener("click", videos);
     document.getElementById("accounts").addEventListener("click", accounts);
-    function gallery(){
+    function photos(){
         const req = new XMLHttpRequest();
         req.onload = function() {
           document.getElementById("main-content").innerHTML = this.responseText;
         }
-        req.open("GET", "gallery.txt");
+        req.open("GET", "photos.txt");
+        req.send();
+    }
+    function videos(){
+        const req = new XMLHttpRequest();
+        req.onload = function() {
+          document.getElementById("main-content").innerHTML = this.responseText;
+        }
+        req.open("GET", "videos.txt");
         req.send();
     }
     function accounts(){
@@ -18,13 +27,9 @@ $(document).ready(function(){
         req.send();
     }
     //REMOVE AND ADD CLASS ATTRIBUTE
-    $("#gallery").click(function(){
-        $("#accounts").removeAttr("class");
-        $("#gallery").attr("class", "active");
-    })
-    $("#accounts").click(function(){
-        $("#gallery").removeAttr("class");
-        $("#accounts").attr("class", "active");
+    $("ul li a").click(function(){
+        $("li a").removeAttr("class");
+        $(this).attr("class", "active");
     })
     //OPEN LINKS
     $(".web").on("click", function(){
@@ -45,46 +50,79 @@ $(document).ready(function(){
     $(".steam").live("click", function(){
         window.open("https://www.steamcommunity.com/id/230658225/", "_blank");
     })
-    //VIEW CONTENT
-    $("img").live("click",function(){
+    //VIEW IMAGES AND VIDEOS
+    $(".photos img, .videos video, .profile-pic img").live("click",function(){
        var content = $(this).clone();
        $(".view").css({
            "visibility" : "visible",
        });
-       $(".view .content").append(content);
-
+       $(".view").append(content);
+       
        if ($(window).width() <= 768) {
-          $(".content").css({
+          $(".view img, .view video").css({
+              "display" : "block",
+              "width" : "100%",
+              "height" : "70%",
+              "object-fit" : "contain",
               "position" : "absolute",
               "top" : "50%",
-              "left" : "50%",
-              "transform" : "translate(-50%, -50%)",
-              "width" : "250px",
-              "height" : "300px",
-          });
-          $(".content img, .content video").css({
-              "width" : "100%",
-              "height" : "100%",
-              "object-fit" : "cover",
+              "transform" : "translateY(-50%)",
+              "background" : "#fff"
           })
+          $(".close").css({
+            "top" : "5%"
+        })
        }
        else {
-          $(".content").css({
+          $(".view img, .view video").css({
+            "display" : "block",
+            "width" : "40%",
+            "height" : "100vh",
+            "object-fit" : "cover",
             "position" : "absolute",
             "left" : "50%",
             "transform" : "translateX(-50%)",
-            "width" : "40%",
-            "height" : "100%",
-          })
-          $(".content img, .content video").css({
-            "width" : "100%",
-            "height" : "100%",
-            "object-fit" : "cover"
           })
        }
-   })
-   $(".view").click(function(){
-       $(".content img, .content video").remove();
-       $(this).css("visibility", "hidden");
-   })
+       var video = $(".view video");
+       $(video).get(0).play();
+       $(video).get(0).currentTime = 0;
+       $(video).get(0).controls = true;
+    })
+    
+   
+   //VIEW COVER IMAGE
+    $(".cover").live("click", function(){
+       var img = $(this).children("img:first-child").clone();
+       $(".view").append(img);
+       $(".view").css("visibility", "visible");
+       if ($(window).width() <= 768) {
+           $(".view img").css({
+               "display" : "block",
+               "position" : "absolute",
+               "width" : "100%",
+               "top" : "50%",
+               "transform" : "translateY(-50%)"
+           })
+           $(".close").css({
+               "top" : "5%"
+           })
+       }
+       else{
+           $(".view img").css({
+               "display" : "block",
+               "position" : "absolute",
+               "height" : "40%",
+               "width" : "100%",
+               "object-fit" : "contain",
+               "top" : "50%",
+               "left" : "50%",
+               "transform" : "translate(-50%, -50%)"
+           })
+       }
+    })
+    $(".close").click(function(){
+        $(".view").css("visibility", "hidden");
+        $(".view img, .view video").remove();
+    })
 })
